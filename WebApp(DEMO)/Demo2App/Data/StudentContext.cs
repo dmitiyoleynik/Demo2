9860e78533d4254d48c64429495df982b65c9efd
Dmitriy <42034186+dmitiyoleynik@users.noreply.github.com>
2019-05-29 12:56:03 +0300

    Add files via upload

---
+++ WebApp(DEMO)/Demo2App/Data/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo2App.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Demo2App.Models.Student> Student { get; set; }
+    }
+}
---
+++ WebApp(DEMO)/Demo2App/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo2App.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+    }
+}
